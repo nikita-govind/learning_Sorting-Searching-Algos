f36bd60f764997edf7d3d3be15e549f2622f05e9
Nikita  Govind <nikitagovind@MacBook-Pro-123.local>
2017-06-12 13:00:06 -0700

    Added a skeleton C++ project. Just has a main with "Hello World".

---
+++ SortingAlgorithms/SortingAlgorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SortingAlgorithms
+//
+//  Created by Nikita  Govind on 6/12/17.
+//  Copyright © 2017 Nikita  Govind. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
